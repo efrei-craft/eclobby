961a6013e93262567ec820929be9105104486339
DocSystem <42613513+DocSystem@users.noreply.github.com>
2022-10-15 17:56:39 +0200

    first commit

---
+++ src/main/java/fr/efreicraft/eclobby/Eclobby.java
@@ -0,0 +1,17 @@
+package fr.efreicraft.eclobby;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Eclobby extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
