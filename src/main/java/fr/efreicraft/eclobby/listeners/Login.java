a0b9c54fb159c442f1a9abe90b8e3b8dac3d03da
DocSystem <42613513+DocSystem@users.noreply.github.com>
2022-10-16 18:33:01 +0200

    Update Login Event
    - Set player game mode to adventure
    - Give him to fly if he has permission to
    - Do all of this after 1 tick

--- src/main/java/fr/efreicraft/eclobby/listeners/Login.java
+++ src/main/java/fr/efreicraft/eclobby/listeners/Login.java
@@ -1,21 +1,24 @@
 package fr.efreicraft.eclobby.listeners;
 
 import fr.efreicraft.eclobby.Main;
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class Login implements Listener {
-    public Login() {
-        Main.INSTANCE.getServer().getPluginManager().registerEvents(this, Main.INSTANCE);
-    }
-
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        if (player.hasPermission("eclobby.fly")) {
-            player.setAllowFlight(true);
-        }
+        Bukkit.getScheduler().runTaskLater(Main.INSTANCE, () -> {
+            player.setGameMode(GameMode.ADVENTURE);
+            if (player.hasPermission("eclobby.fly")) {
+                player.setAllowFlight(true);
+            }
+            player.teleport(new Location(player.getWorld(), 0.5, 1, 0.5, 90, 0));
+        }, 1L);
     }
 }
