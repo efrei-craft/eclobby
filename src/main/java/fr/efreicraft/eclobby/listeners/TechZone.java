96b76fed09f502a61c0fa82c32bbc8b9c559dc6a
DocSystem <42613513+DocSystem@users.noreply.github.com>
2022-10-17 21:02:17 +0200

    Add player walk on JIGSAW block event
    - Push player when he walks on a jigsaw block, depending on the direction he's looking at

--- src/main/java/fr/efreicraft/eclobby/listeners/TechZone.java
+++ src/main/java/fr/efreicraft/eclobby/listeners/TechZone.java
@@ -1,6 +1,32 @@
 package fr.efreicraft.eclobby.listeners;
 
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
 
 public class TechZone implements Listener {
+    @EventHandler
+    public void onPlayerWalkOnBlock(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
+        Location loc = player.getLocation();
+        Location blockUnderPlayer = new Location(loc.getWorld(), loc.getX(), loc.getY() - 1, loc.getZ());
+        if (blockUnderPlayer.getBlock().getType() == Material.JIGSAW) {
+            float playerYaw = loc.getYaw();
+            if (playerYaw > -45 && playerYaw < 45) { // player looking at south - Z+
+                player.setVelocity((new Location(player.getWorld(), loc.getX(), loc.getY(), loc.getZ(), 0, -30)).getDirection());
+            }
+            else if (playerYaw > 45 && playerYaw < 135) { // player looking at east - X+
+                player.setVelocity((new Location(player.getWorld(), loc.getX(), loc.getY(), loc.getZ(), 90, -30)).getDirection());
+            }
+            else if (playerYaw > 135 || playerYaw < -135) { // player looking at north - Z-
+                player.setVelocity((new Location(player.getWorld(), loc.getX(), loc.getY(), loc.getZ(), 180, -30)).getDirection());
+            }
+            else { // player looking at west - X-
+                player.setVelocity((new Location(player.getWorld(), loc.getX(), loc.getY(), loc.getZ(), -90, -30)).getDirection());
+            }
+        }
+    }
 }
