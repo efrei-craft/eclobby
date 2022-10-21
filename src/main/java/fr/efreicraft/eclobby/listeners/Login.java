8f27ba202a9b0e31aaed89b32d07b22c082b5818
Niilyx _ <nait.meddour.idir100@gmail.com>
2022-10-21 23:52:18 +0200

    set boussole à l'arrivée du joueur dans le serv

--- src/main/java/fr/efreicraft/eclobby/listeners/Login.java
+++ src/main/java/fr/efreicraft/eclobby/listeners/Login.java
@@ -1,15 +1,29 @@
 package fr.efreicraft.eclobby.listeners;
 
 import fr.efreicraft.eclobby.Main;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.CompassMeta;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.Collections;
+
+import static fr.efreicraft.eclobby.Main.colorize;
 
 public class Login implements Listener {
+
+    public static ItemStack menuBoussole;
+
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
@@ -27,5 +41,7 @@ public class Login implements Listener {
                 player.teleport(new Location(player.getWorld(), 0.5, 1, 0.5, 90, 0));
             }
         }, 0L, 20L);
+        
+        player.getInventory().setItem(0, menuBoussole);
     }
 }
