1650f519ab253c48647989c4e324884aa380f1e0
Niilyx <nait.meddour.idir100@gmail.com>
2022-10-28 13:30:18 +0200

    opti imports

--- src/main/java/fr/efreicraft/eclobby/listeners/Login.java
+++ src/main/java/fr/efreicraft/eclobby/listeners/Login.java
@@ -1,24 +1,14 @@
 package fr.efreicraft.eclobby.listeners;
 
 import fr.efreicraft.eclobby.Main;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
-import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.CompassMeta;
-import org.bukkit.inventory.meta.ItemMeta;
-
-import java.util.Collections;
-
-import static fr.efreicraft.eclobby.Main.colorize;
 
 public class Login implements Listener {
 
