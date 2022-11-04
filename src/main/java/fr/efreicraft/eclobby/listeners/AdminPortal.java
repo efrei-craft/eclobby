82547bf3c671d1d3f359ce8606ed69e707c7b03a
Niilyx _ <nait.meddour.idir100@gmail.com>
2022-11-04 18:42:06 +0100

    no more playerportalevent :pensive:

--- src/main/java/fr/efreicraft/eclobby/listeners/AdminPortal.java
+++ src/main/java/fr/efreicraft/eclobby/listeners/AdminPortal.java
@@ -1,6 +1,7 @@
 package fr.efreicraft.eclobby.listeners;
 
 import fr.efreicraft.eclobby.utils.Utils;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.event.EventHandler;
@@ -14,11 +15,9 @@ import static fr.efreicraft.eclobby.Main.INSTANCE;
 public class AdminPortal implements Listener {
 
     @EventHandler
-    public void onGoToPortal(PlayerPortalEvent event) {
+    public void onGoToPortal(PlayerMoveEvent event) {
         World overworld = event.getPlayer().getWorld();
         if (overworld.getEnvironment() != World.Environment.NORMAL) return;
-
-        event.setCancelled(true);
 
         Location loc = event.getPlayer().getLocation();
         float yaw = loc.getYaw();
@@ -40,14 +39,14 @@ public class AdminPortal implements Listener {
 
         // Aller-retour pôle nether.
         else if (Utils.isBlockInside(loc, new Location(overworld, -1, -3, -12), 2, 2, 0)) {
-            if (Utils.inRange(yaw, 0, 180)) yaw = -90;
+            yaw = -90;
 
             event.getPlayer().teleport(new Location(event.getPlayer().getWorld(), -47, -1, -47.5, yaw, pitch));
             INSTANCE.getServer().getScheduler().runTaskLaterAsynchronously(INSTANCE, () -> event.getPlayer().setVelocity(vel), 1L);
         } else if (Utils.isBlockInside(loc, new Location(overworld, -48, -1, -48), 0, 2, 2)) {
-            if (Utils.inRange(yaw, -90, 90)) yaw = 0;
+            yaw = 0;
 
-            event.getPlayer().teleport(new Location(event.getPlayer().getWorld(), -0.5, -3, -11, yaw, pitch));
+            event.getPlayer().teleport(new Location(event.getPlayer().getWorld(), 0.5, -3, -11, yaw, pitch));
             INSTANCE.getServer().getScheduler().runTaskLater(INSTANCE, () -> event.getPlayer().setVelocity(vel), 1L);
         }
     }
