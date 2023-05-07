7e6dd6d7fb1477bd259f6d2b0bcb871034c9528e
Niilyx _ <nait.meddour.idir100@gmail.com>
2023-05-08 00:55:10 +0200

    Usage of MessageUtils

--- src/main/java/fr/efreicraft/eclobby/commands/Lobby.java
+++ src/main/java/fr/efreicraft/eclobby/commands/Lobby.java
@@ -1,6 +1,6 @@
 package fr.efreicraft.eclobby.commands;
 
-import org.bukkit.ChatColor;
+import fr.efreicraft.ecatup.utils.MessageUtils;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -13,12 +13,12 @@ public class Lobby implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (sender instanceof Player player) {
-            player.sendMessage(ChatColor.GREEN + "Vous avez été téléporté au lobby !");
+            MessageUtils.sendMessage(player, "&aVous avez été téléporté au lobby !");
             player.teleport(new Location(player.getWorld(), 0.5, 1, 0.5, 90, 0));
             return true;
         }
         else {
-            sender.sendMessage(ChatColor.RED + "Vous devez être un joueur pour exécuter cette commande !");
+            MessageUtils.sendMessage(sender, "&cVous devez être un joueur pour exécuter cette commande !");
             return false;
         }
     }
