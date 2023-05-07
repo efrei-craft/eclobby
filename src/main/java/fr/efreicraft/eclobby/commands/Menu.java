3b618e1eadf965ae9c6fb5d1c6e338841e849746
Niilyx _ <nait.meddour.idir100@gmail.com>
2023-05-08 00:56:10 +0200

    Usage of variable for y threshold

--- src/main/java/fr/efreicraft/eclobby/commands/Menu.java
+++ src/main/java/fr/efreicraft/eclobby/commands/Menu.java
@@ -1,14 +1,32 @@
 package fr.efreicraft.eclobby.commands;
 
+import fr.efreicraft.animus.endpoints.PlayerService;
+import fr.efreicraft.animus.invoker.ApiException;
+import fr.efreicraft.ecatup.players.ECPlayer;
+import fr.efreicraft.ecatup.utils.MessageUtils;
+import fr.efreicraft.eclobby.interfaces.Boussole;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 public class Menu implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (sender instanceof Player player) {
+            //TODO mettre en cache le player pour éviter les calls de DB...
+            try {
+                fr.efreicraft.animus.models.Player animusPlayer = PlayerService.getPlayer(player.getUniqueId().toString());
+                Boussole.openBoussole(new ECPlayer(player, animusPlayer));
+                return true;
+            } catch (ApiException e) {
+                MessageUtils.sendMessage(sender, "&cNous n'avons pas pu récupérer vos données pour ouvrir le menu.");
+            }
+        } else {
+            MessageUtils.sendMessage(sender, "&cVous devez être un joueur pour exécuter cette commande !");
+        }
         return false;
     }
 }
