e72bf063972de4f67f2f497e6f2260cb5445e9c8
JiveOff <antoine@jiveoff.fr>
2023-10-07 23:50:30 +0200

    🗒️ 1.20.2+

--- src/main/java/fr/efreicraft/eclobby/interfaces/Boussole.java
+++ src/main/java/fr/efreicraft/eclobby/interfaces/Boussole.java
@@ -78,7 +78,7 @@ public class Boussole {
                     31,
                     () -> {
                         // FIXME: hardcodé.
-                        String description = "&7Une survie simple, comme vous l'aimez !\n\n&7Reservé aux joueurs en &c1.20.1+&7.";
+                        String description = "&7Une survie simple, comme vous l'aimez !\n\n&7Reservé aux joueurs en &c1.20.2+&7.";
                         try {
                             Server vanillaServer = ServerService.getServer(System.getenv("INFRASTRUCTURE_NAME") + ".vanilla");
 
